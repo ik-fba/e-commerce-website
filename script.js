0ac321154b3b852ddcf33769410e3f2f54708368
Ibrahim Kone <ikone10@mail.com>
2023-08-20 17:17:28 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
 
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', function(event) {
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", function (event) {
     event.preventDefault();
-    button.innerHTML = 'Added to Cart!';
-    button.classList.add('added');
+    button.innerHTML = "Added to Cart!";
+    button.classList.add("added");
 
     setTimeout(() => {
       button.innerHTML = 'Add to Cart <i class="bx bx-cart"></i>';
-      button.classList.remove('added');
+      button.classList.remove("added");
     }, 3000);
   });
 });
@@ -16,7 +16,7 @@ addToCartButtons.forEach(button => {
 let menu = document.getElementById("menu");
 let navbar = document.querySelector(".navmenu");
 
-menu.onclick = function(){
+menu.onclick = function () {
   menu.classList.toggle("openmenu");
-  navbar.classList.toggle('open');
+  navbar.classList.toggle("open");
 };
